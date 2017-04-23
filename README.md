b2ee64383fe68faf55ae01d1c19899c5cd775b2a
Michał Mazurek <michalgmazurek@gmail.com>
2017-04-23 14:55:11 +0200

    Responsive

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Pong-Phaser
-
-
+My Project: https://michalgmazurek.github.io/Pong-Phaser/
 <br><br>
-https://michalgmazurek.github.io/Pong-Phaser/
+Sound: https://www.freesound.org/people/zagi2/sounds/339124/
+<br>
+Ball png: http://nintendo.wikia.com/wiki/File:MTO_Tennis_Ball.png
--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
+
+    var game = new Phaser.Game(400, 300, Phaser.AUTO, '', {
         preload: preload
         , create: create
         , update: update
     });
-    
-    
+
     var paddle1;
     var paddle2;
     var ball;
@@ -17,6 +16,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var score1;
     var score2;
     var gameOver;
+    var wievportWidth = parseInt(window.innerWidth);
+    var wievportHeight = parseInt(window.innerHeight);
 
     function preload() {
         game.load.image('paddle', 'assets/paddle.png');
@@ -26,17 +27,29 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
 
     function create() {
-        game.stage.backgroundColor = '#666';
+        game.stage.backgroundColor = '#3c4240';
         paddle1 = createPaddle(0, game.world.centerY);
-        paddle2 = createPaddle(game.world.width - 3, game.world.centerY);
+        paddle2 = createPaddle(game.world.width, game.world.centerY);
         ballLaunched = false;
-        ballVelocity = 350;
+        ballVelocity = 280;
         ball = createBall(game.world.centerX, game.world.centerY);
         game.input.onDown.add(launchBall, this);
-        score1Text = game.add.bitmapText(128, 28, 'font', '0', 40);
-        score2Text = game.add.bitmapText(game.world.width - 128, 28, 'font', '0', 40);
+        score1Text = game.add.bitmapText(64, 14, 'font', '0', 20);
+        score2Text = game.add.bitmapText(game.world.width - 64, 14, 'font', '0', 20);
         score1 = 0;
         score2 = 0;
+        var canvas = document.querySelector("canvas");
+        console.log(parseInt(canvas.style.width));
+        if (wievportHeight > wievportWidth ) {
+            var canvasWidth = parseInt(canvas.style.width);
+            var xy = (wievportWidth / canvasWidth) * 0.9;
+            canvas.style.transform = "scale(" + xy + ")";
+        }
+        else if (wievportHeight < wievportWidth ) {
+            var canvasHeight = parseInt(canvas.style.height);
+            var xy = (wievportHeight / canvasHeight) * 0.9;
+            canvas.style.transform = "scale(" + xy + ")";
+        }
     }
 
     function update() {
@@ -53,37 +66,37 @@ document.addEventListener("DOMContentLoaded", function (event) {
             score1 += 1;
             launchBall();
         }
-       
+
         paddle2.body.velocity.setTo(ball.body.velocity.y-50);
         paddle2.body.velocity.x = 0;
-        paddle2.body.maxVelocity.y = 700;
-        paddle1.scale.setTo(0.7, 0.7);
-        paddle2.scale.setTo(0.7, 0.7);
-        ball.scale.setTo(0.7, 0.7);
-        
+        paddle2.body.maxVelocity.y = 500;
+        paddle1.scale.setTo(0.4, 0.4);
+        paddle2.scale.setTo(0.4, 0.4);
+        ball.scale.setTo(0.4, 0.4);
+
          if (score1 > 0) {
-            ballVelocity = 500;
-             paddle2.body.velocity.setTo(ball.body.velocity.y-52);
+            ballVelocity = 400;
+             paddle2.body.velocity.setTo(ball.body.velocity.y-50);
              paddle2.body.velocity.x = 0;
         }
         if (score1 > 1) {
-            ballVelocity = 600;
-            paddle2.body.velocity.setTo(ball.body.velocity.y-60);
+            ballVelocity = 410;
+            paddle2.body.velocity.setTo(ball.body.velocity.y-67);
             paddle2.body.velocity.x = 0;
         }
         if (score1 > 2) {
-            gameOver = game.add.bitmapText(160, 250, 'font', 'WINNER!', 70);
+            gameOver = game.add.bitmapText(70, 125, 'font', 'WINNER!', 40);
             game.input.onDown.remove(launchBall, this);
         }
         if (score2 > 2) {
-            gameOver = game.add.bitmapText(90, 250, 'font', 'GAME OVER', 70);
+            gameOver = game.add.bitmapText(20, 125, 'font', 'GAME OVER', 40);
             game.input.onDown.remove(launchBall, this);
         }
     }
 
     function createPaddle(x, y) {
         var paddle = game.add.sprite(x, y, 'paddle');
-        paddle.anchor.setTo(0.5, 0.5);
+        paddle.anchor.setTo(0.1, 0.5);
         game.physics.arcade.enable(paddle);
         paddle.body.collideWorldBounds = true;
         paddle.body.immovable = true;
@@ -122,4 +135,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
             ballLaunched = true;
         }
     }
+
+    // var canvas = document.querySelector("ctx.canvas");
+    // var wievportWidth = parseInt(window.innerWidth);
+    // var wievportHeight = parseInt(window.innerHeight);
+    console.log(wievportWidth, wievportHeight);
+
+    // if (wievportWidth > wievportHeight) {
+    //     var xy = wievportHeight/wievportWidth;
+    //     canvas.style.transform = "scale(" + xy + ")"
+    // }
+
+
 });
